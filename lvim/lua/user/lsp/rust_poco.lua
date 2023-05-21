4db09bc1ee3cb6d43047171292c7986905a8a18b
helyes <helyesa@gmail.com>
2023-05-21 15:43:35 +0900

    Removed duplicated (identical to default) settings from poko

--- lvim/lua/user/lsp/rust_poco.lua
+++ lvim/lua/user/lsp/rust_poco.lua
@@ -3,45 +3,47 @@
 -- start general pojok code
 local on_save = true
 local options = {
-  backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
+  -- backspace lvim default: indent,eol,start
+  -- backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
   -- clipboard = "unnamedplus", -- Connection to the system clipboard
-  cmdheight = 0, -- hide command line unless needed
-  completeopt = { "menuone", "noselect" }, -- Options for insert mode completion
+  -- cmdheight lvim default: 1 --  -- more space in the neovim command line for displaying messages
+  -- cmdheight = 0, -- hide command line unless needed
+  -- same as default  completeopt = { "menuone", "noselect" }, -- Options for insert mode completion
   copyindent = true, -- Copy the previous indentation on autoindenting
-  cursorline = true, -- Highlight the text line of the cursor
-  expandtab = true, -- Enable the use of space in tab
-  fileencoding = "utf-8", -- File content encoding for the buffer
+  -- not needed, set somewhere else cursorline = true, -- Highlight the text line of the cursor
+  -- same as default expandtab = true, -- Enable the use of space in tab
+  -- same as default fileencoding = "utf-8", -- File content encoding for the buffer
   fillchars = { eob = " " }, -- Disable `~` on nonexistent lines
-  history = 100, -- Number of commands to remember in a history table
-  ignorecase = true, -- Case insensitive searching
-  laststatus = 3, -- globalstatus
+  -- default is 10000, no need to change history = 100, -- Number of commands to remember in a history table
+  -- same as default ignorecase = true, -- Case insensitive searching
+  -- same as defult laststatus = 3, -- globalstatus
   lazyredraw = true, -- lazily redraw screen
-  mouse = "a", -- Enable mouse support
-  number = true, -- Show numberline
-  preserveindent = true, -- Preserve indent structure as much as possible
-  pumheight = 10, -- Height of the pop up menu
-  relativenumber = true, -- Show relative numberline
-  scrolloff = 8, -- Number of lines to keep above and below the cursor
-  shiftwidth = 2, -- Number of space inserted for indentation
+  -- same as default mouse = "a", -- Enable mouse support
+  -- same as default number = true, -- Show numberline
+  -- same as default preserveindent = true, -- Preserve indent structure as much as possible
+  -- same as default pumheight = 10, -- Height of the pop up menu
+  -- same as default relativenumber = true, -- Show relative numberline
+  -- same as default scrolloff = 8, -- Number of lines to keep above and below the cursor
+  -- same as default shiftwidth = 2, -- Number of space inserted for indentation
   showmode = false, -- Disable showing modes in command line
-  showtabline = 2, -- always display tabline
-  sidescrolloff = 8, -- Number of columns to keep at the sides of the cursor
-  signcolumn = "yes", -- Always show the sign column
-  smartcase = true, -- Case sensitivie searching
-  splitbelow = true, -- Splitting a new window below the current one
-  splitright = true, -- Splitting a new window at the right of the current one
-  swapfile = false, -- Disable use of swapfile for the buffer
-  tabstop = 2, -- Number of space in a tab
-  termguicolors = true, -- Enable 24-bit RGB color in the TUI
-  timeoutlen = 300, -- Length of time to wait for a mapped sequence
-  undofile = true, -- Enable persistent undo
-  updatetime = 300, -- Length of time to wait before triggering the plugin
+  -- same as default showtabline = 2, -- always display tabline
+  -- same as default sidescrolloff = 8, -- Number of columns to keep at the sides of the cursor
+  -- same as default signcolumn = "yes", -- Always show the sign column
+  -- same as default smartcase = true, -- Case sensitivie searching
+  -- same as default splitbelow = true, -- Splitting a new window below the current one
+  -- same as default splitright = true, -- Splitting a new window at the right of the current one
+  -- same as default swapfile = false, -- Disable use of swapfile for the buffer
+  -- same as default tabstop = 2, -- Number of space in a tab
+  -- same as default termguicolors = true, -- Enable 24-bit RGB color in the TUI
+  timeoutlen = 300, -- Length of time to wait for a mapped sequence -- default: 200
+  -- same as default undofile = true, -- Enable persistent undo
+  updatetime = 300, -- this many milliseconds nothing is typed the swap file will be written to disk
   wrap = true, -- Disable wrapping of lines longer than the width of window
-  writebackup = false, -- Disable making a backup before overwriting a file
+  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   -- minimal number of screen columns either side of cursor if wrap is `false`
   -- guifont = "monospace:h17", -- the font used in graphical neovim applications
   guifont = "Source_Code_Pro:h17", -- the font used in graphical neovim applications
-  whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line            -- which "horizontal" keys are allowed to travel to prev/next line
+  -- not needed, using defaults whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
 }
 for k, v in pairs(options) do
   vim.opt[k] = v
