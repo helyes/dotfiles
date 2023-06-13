575169bf541685e350f2cc9801d1d88abad16d1c
helyes <helyesa@gmail.com>
2023-06-14 08:13:08 +0900

    Plugin: nvim-ts-rainbow2

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -39,6 +39,9 @@ lvim.plugins = {
     url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   },
   {
+  "HiPhish/nvim-ts-rainbow2",
+  },
+  {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
     ft = "markdown",
