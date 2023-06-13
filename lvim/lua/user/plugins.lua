35757fe00be5f8ba84e2e26063c4de7cce8f2c83
helyes <helyesa@gmail.com>
2023-06-13 11:37:55 +0900

    Added lsp_lines

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -33,6 +33,9 @@ lvim.plugins = {
     dependencies = { "nvim-lua/plenary.nvim" },
   },
   {
+    url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  },
+  {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
     ft = "markdown",
