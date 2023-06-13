8c7d7e3849bc5f23da21ee435610e3a5b82f6c06
helyes <helyesa@gmail.com>
2023-06-14 08:36:37 +0900

    Added autocommands

---
+++ lvim/lua/user/autocommands.lua
@@ -0,0 +1,39 @@
+-- remap "q" to close window
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "Jaq",
+    "qf",
+    "help",
+    "man",
+    "lspinfo",
+    "spectre_panel",
+    "lir",
+    "DressingSelect",
+    "tsplayground",
+    "",
+  },
+  callback = function()
+    vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
+    ]]
+  end,
+})
+
+-- remove trailing whitespaces on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = {
+    "*.fish",
+    "*.js",
+    "*.lua",
+    "*.md",
+    "*.rs",
+    "*.tf",
+    "*.ts",
+  },
+  callback = function()
+    vim.cmd [[
+      :%s/\s\+$//e
+    ]]
+  end,
+})
