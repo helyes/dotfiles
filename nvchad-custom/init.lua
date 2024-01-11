4dc26d6e395b528a85724354cfc8cf75490bb399
helyes <helyesa@gmail.com>
2024-01-11 18:01:32 +0900

    NV: Highlight yank

--- nvchad-custom/init.lua
+++ nvchad-custom/init.lua
@@ -5,3 +5,13 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+--
+vim.opt.relativenumber = true
+
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+" au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+au TextYankPost * silent! lua vim.highlight.on_yank({timeout=200})
+augroup END
+]]
