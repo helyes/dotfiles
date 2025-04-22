9f248de819b8e479de15d917834ef4da6542fabd
helyes <helyesa@gmail.com>
2025-04-22 15:32:23 +0900

    NVIM: new opts for NvChad

--- nvchad25/lua/options.lua
+++ nvchad25/lua/options.lua
@@ -5,3 +5,11 @@ require "nvchad.options"
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.clipboard = ""
+
+o.relativenumber = true
+
+-- to show end of line and dots on space
+vim.opt.list = true
+vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "trail:⋅"
+vim.opt.listchars:append "lead:⋅"
