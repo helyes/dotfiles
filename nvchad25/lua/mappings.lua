b10557b284249a694bc915a2cd358317336db985
helyes <helyesa@gmail.com>
2026-01-05 15:04:25 +0900

    Nvchad: mapping and lazylock

--- nvchad25/lua/mappings.lua
+++ nvchad25/lua/mappings.lua
@@ -1,5 +1,5 @@
 require "nvchad.mappings"
-
+require "overrides.mappings"
 -- add yours here
 
 local map = vim.keymap.set
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- telescope
+map("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "telescope keymaps" })
+-- local nomap = vim.keymap.del
+
+-- nomap("n", "<leader>b")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
